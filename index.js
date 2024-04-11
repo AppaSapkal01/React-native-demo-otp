16c73cbb00aded9b7af4affcbe06dc1dff2c4bdb
AppaSapkal01 <appasapkal26977@gmail.com>
2024-04-11 17:23:11 +0530

    setup the otp funcationality

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { getFireStore } from '@react-native-firebase/firestore'
+import { getAuth } from '@react-native-firebase/auth'
+import { initializeApp } from '@react-native-firebase/app';
+
+// Initialize Firebase
+initializeApp();
+
 
 AppRegistry.registerComponent(appName, () => App);
